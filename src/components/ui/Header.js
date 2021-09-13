be388030ae9a7bd1ae578b303ab24ba00067e875
S Varez <varezsergio@gmail.com>
2021-09-13 17:13:37 +0200

    linking logo 2

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 export const Header = () => {
+    
+
     return (
         <div className="header">
-            <a href="./musigame/"><h2>musigame.</h2></a>
+            <Link to="/musigame"><h2>musigame.</h2></Link>
         </div>
     )
 }
