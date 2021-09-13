6c8b7b7e1b20f14a1fa6705fc5d4fc0220e31215
S Varez <varezsergio@gmail.com>
2021-09-13 17:00:29 +0200

    link in logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div className="header">
-            <h2>musigame.</h2>
+            <a href="./musigame/"><h2>musigame.</h2></a>
         </div>
     )
 }
